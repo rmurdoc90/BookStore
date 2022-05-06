513cd304a8860a1b8d981aa4cbebb9873635b3da
Robert Murdoch <rmurdoc90@gmail.com>
2022-05-06 16:28:27 -0700

    5/6/22 EOD Final commit

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -17,5 +17,10 @@ namespace BookStoreWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
     }
 }
