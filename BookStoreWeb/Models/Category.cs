b2e3c1bdfac8abfba9e7d32ab9d7f398c17bcf35
Robert Murdoch <rmurdoc90@gmail.com>
2022-05-16 15:23:28 -0700

    added display Name and Roange to Display order property

--- BookStoreWeb/Models/Category.cs
+++ BookStoreWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStoreWeb.Models
 {
@@ -8,6 +9,9 @@ namespace BookStoreWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
         public DateTime MyProperty { get; set; } = DateTime.Now;
     }
