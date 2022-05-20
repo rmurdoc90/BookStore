f013a30986fed36c9b9d8a67ecabec30d311ad38
Robert Murdoch <rmurdoc90@gmail.com>
2022-05-19 21:06:11 -0700

    Completed project

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -12,6 +12,8 @@ namespace BookStoreWeb.Controllers
         {
             _db = db;
         }
+
+        // Get
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
@@ -44,6 +46,7 @@ namespace BookStoreWeb.Controllers
             return View(obj);
         }
 
+        // Get
         public IActionResult Edit(int? id)
         {
             if(id== null || id == 0)
@@ -79,6 +82,7 @@ namespace BookStoreWeb.Controllers
             return View(obj);
         }
 
+        // Get
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
